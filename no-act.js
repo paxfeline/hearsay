43b128b0b558f47d691539e8e1a89f59a4052626
paxfeline <paxfeline@gmail.com>
2025-01-26 13:24:35 -0800

    added init

--- no-act.js
+++ no-act.js
@@ -1,40 +1,86 @@
+window.noAct = {
+    init(opts)
+    {
+        console.log("init", opts, this.elements.current);
+        Object.assign(this.elements.current, opts);
+        this.elements.current.opts = opts;
+        opts.init?.(this.elements.current);
+    }
+};
+
 class DataConsumer extends HTMLElement {
     constructor()
     {
-      super();
-
-      /* MDN suggests this maybe better in connectedCallback
-        but I think it's ok here because it doesn't matter
-        because... */
-      
-      fetch(this.dataset.struct)
-          .then( res => res.text() )
-          .then( txt => this.attachShadow({mode: "open"}).innerHTML = txt )
-          .then( () =>
-            {
-                window.noAct = window.noAct || {};
-                noAct.elements = {current: this, previous: noAct.currentElement};
-                const oldScript = this.shadowRoot.querySelector("script");
-                if (oldScript)
-                {
-                    const newScript = document.createElement("script");
-                    newScript.textContent = oldScript.textContent;
-                    oldScript.replaceWith(newScript);
-                }
-                noAct.elements = noAct.elements.previous;
-            } )
+        super();
     }
 
     connectedCallback()
-    {   
+    {
+        fetch(this.dataset.struct)
+        .then( res => res.text() )
+        .then( txt => this.attachShadow({mode: "open"}).innerHTML = txt )
+        .then( () =>
+        {
+            noAct.elements = {current: this, previous: noAct.elements?.current};
+            const oldScript = this.shadowRoot.querySelector("script");
+            if (oldScript)
+            {
+                const newScript = document.createElement("script");
+                newScript.textContent = oldScript.textContent;
+                oldScript.replaceWith(newScript);
+            }
+            noAct.elements = noAct.elements.previous;
+        } )
+        this.opts?.connectedCallback?.();
     }
+
+    disconnectedCallback()
+    {
+        this.opts?.disconnectedCallback?.();
+    }
+
+    adoptedCallback()
+    {
+        this.opts?.adoptedCallback?.();
+    }
+
+    attributeChangedCallback()
+    {
+        this.opts?.attributeChangedCallback?.();
+    }
+
+    react(data)
+    {
+        this.opts?.react?.(this, data);
+    }
+
+    /* util functions */
+
+    slot(name, data)
+    {
+        // if no data to set, return value
+        if (data === undefined) return this.querySelector(`[slot="${name}"]`).children;
+
+        if (!Array.isArray(data)) data = [data];
+        this.querySelector(`[slot="${name}"]`).replaceChildren(...data
+            .map( el => el.nodeType ? el : document.createTextNode(el) )
+        );
+    }
+
+    static observedAttributes = ["props"];
 }
 
 customElements.define("data-consumer", DataConsumer);
 
-function broadcastToConsumers(data, tos)
+function broadcast(data, recipients)
 {
-    const allConsumers = document.querySelectorAll("")
+    // data-consumer elements
+    const allConsumerElements = document.querySelectorAll("data-consumer");
+    allConsumerElements.forEach( consumer => consumer.react(consumer, data, recipients) );
+    
+    // elements with data-consumer attribute
+    const allConsumerCallbacks = document.querySelectorAll("[data-consumer]");
+    allConsumerCallbacks.forEach( consumer => consumer(consumer, data, recipients) );
 }
 
-export { DataConsumer };
+noAct.broadcast = broadcast;
