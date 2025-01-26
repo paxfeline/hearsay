4acaed4cfa0406d362253a0db7d0593d8ba792fe
paxfeline <paxfeline@gmail.com>
2025-01-25 23:44:59 -0800

    allow script elements

--- no-act.js
+++ no-act.js
@@ -2,20 +2,39 @@ class DataConsumer extends HTMLElement {
     constructor()
     {
       super();
-    }
-    connectedCallback()
-    {
-        fetch(this.dataset.struct)
-            .then( res => res.text() )
-            .then( txt =>
+
+      /* MDN suggests this maybe better in connectedCallback
+        but I think it's ok here because it doesn't matter
+        because... */
+      
+      fetch(this.dataset.struct)
+          .then( res => res.text() )
+          .then( txt => this.attachShadow({mode: "open"}).innerHTML = txt )
+          .then( () =>
             {
-                const root = this.attachShadow({mode: "open"});
-                root.innerHTML = txt;
+                window.noAct = window.noAct || {};
+                noAct.elements = {current: this, previous: noAct.currentElement};
+                const oldScript = this.shadowRoot.querySelector("script");
+                if (oldScript)
+                {
+                    const newScript = document.createElement("script");
+                    newScript.textContent = oldScript.textContent;
+                    oldScript.replaceWith(newScript);
+                }
+                noAct.elements = noAct.elements.previous;
             } )
-        
+    }
+
+    connectedCallback()
+    {   
     }
 }
 
 customElements.define("data-consumer", DataConsumer);
+
+function broadcastToConsumers(data, tos)
+{
+    const allConsumers = document.querySelectorAll("")
+}
 
 export { DataConsumer };
