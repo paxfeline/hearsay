b4011c396f160b9e45876f391e37e5298986aa40
paxfeline <paxfeline@gmail.com>
2025-02-01 13:51:40 -0800

    progress, but at what cost?

--- hearsay.js
+++ hearsay.js
@@ -163,75 +163,68 @@ class HearSay extends HTMLElement
         // TODO: add component, props parameters
         const prop_att = this.getAttribute("props")?.trim() || "{}";
         const prop_func = Function("self", `return ${prop_att};`);
-
-        /* what follows is like this: 
-            First, the props attribute is evaluated for a returned object,
-            which is then combined with propsData, which is programmatically
-            set via the props setter.
-            A proxy is made for the composite object, with a special getting and setter.
-            One thing it does is, if an object property of props is being returned,
-            it wraps the object in the same proxy. So when accessing nested object,
-            each gets wrapped in the proxy before being returned.
-            But why? The getter also prepends information about the current
-            getter call to the getPath array.
-            This means that if you write:
-                this.props.foo.bar.baz = 42;
-            in the process, first the props() getter is called.
-            It generates the composite object, wraps it in a proxy, and returns it.
-            getPath starts out as [{target: self, prop: "props"}]
-            Then, as the code is evaluated, the proxy's getter method is called.
-            It prepends its information, so getPath will become
-            [{target: (object), prop: "foo"}, {target: self, prop: "props"}]
-            And as this process continues the array grows to:
-                [
-                    {target: (foo object), prop: "bar"},
-                    {target: (props object), prop: "foo"},
-                    {target: this, prop: "props"}
-                ]
-            Finally code execution reaches the equals sign and the setter is called.
-            First, the setter does its job: sets the value of the property on the target.
-            Then, it's time to go backwards:
-            The reduce function is used to iterate the getPath array.
-            First the current target, {baz: 42}, is assigned foo.bar,
-                the foo object is assigned to props.foo, and finally
-                the props object is assigned to this.props.
-            
-            All this so that users can mutate the props object,
-            and have the changes be reflected
-            (instead of having to replace the props object).
-        */
        
-        const propsObj = { ...prop_func(this), ...(this.propsData || {}) };
-       
-        const getPath = [{target: this, prop: "props"}];
-        const proxy = (function makeProxy(obj)
+        function makePropsPropsProxy(propsChain, propsDataChain, prop)
         {
-            const propsHandler =
+            // props[Chain] is functionally immutable
+            propsChain[prop] = propsChain[prop];
+            // if this propsData subobject doesn't exist, create an empty one
+            propsDataChain[prop] = propsDataChain[prop] || {};
+            const targetObj = { props: propsChain[prop], propsData: propsDataChain[prop] }
+            const propsPropsProxyHandler =
             {
-                get(target, prop)
+                get(target, pprop)
                 {
-                    if (typeof target[prop] == "object")
-                    {
-                        getPath.unshift({target, prop})
-                        return makeProxy(target[prop])
-                    }
+                    const val = target.props[pprop] || target.propsData[pprop];
+                    //const val = propsDataChain[prop][pprop] || propsChain[prop][pprop];
+                    if (typeof val == "object")
+                        return makePropsPropsProxy(target.props, target.propsData, pprop)
+                        //return makePropsPropsProxy(propsChain[prop], propsDataChain[prop], pprop)
                     else
-                        return target[prop];
+                        return val;
                 },
 
-                set(target, prop, value)
+                set(target, pprop, val, receiver)
                 {
-                    target[prop] = value;
-                    getPath.reduce(
-                        (acc, {target: ptarget, prop: pprop}) => (ptarget[pprop] = acc, ptarget),
-                        target
-                    );
+                    console.log("set prop proxy prop", target, pprop, val, receiver);
+                    target.propsData[pprop] = val;
+                    self.props = propsProxyTarget.propsData;
                 }
             }
-            const retproxy = new Proxy(obj, propsHandler);
-            return retproxy;
-        })
-        (propsObj);
+            const proxy = new Proxy(targetObj, propsPropsProxyHandler);
+            return proxy;
+        }
+
+        const self = this;
+        // create because may be needed
+        if (!this.propsData) this.propsData = {};
+        const propsProxyTarget = { props: prop_func(this), propsData: this.propsData };
+        const propsProxyHandler = {
+            get(target, prop)
+            {
+                console.log("props proxy get", this);
+                // get prop, try from propsData first, then props
+                let propsDataVal = target.propsData[prop];
+                let propsVal = target.props[prop];
+                const val = propsDataVal || propsVal;
+                if (typeof val == "object")
+                {
+                    // if blah doesn't exist, create an empty one
+                    return makePropsPropsProxy(target.props, target.propsData, prop);
+                    //return makePropsPropsProxy({props: propsVal, propsData: propsDataVal});
+                }
+                // else:
+                return val;
+            },
+            set(target, prop, val, receiver)
+            {
+                console.log("set prop", target, prop, val, receiver);
+                target.propsData[prop] = val;
+                self.props = self.propsData;
+            }
+        }
+
+        const proxy = new Proxy(propsProxyTarget, propsProxyHandler);
 
         return proxy;
     }
